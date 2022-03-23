5ef4031f14d881f839912036c5a46cafbaf5710b
AmandaGAB <amandagabsa@gmail.com>
2022-03-22 23:51:04 -0300

    Alterado

--- src/br/edu/ifpb/gugawag/so/sockets/Client.java
+++ src/br/edu/ifpb/gugawag/so/sockets/Client.java
@@ -3,16 +3,41 @@ package br.edu.ifpb.gugawag.so.sockets;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 
 public class Client {
 
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 7000);
-        Scanner in = new Scanner(socket.getInputStream());
-        while(in.hasNextLine()) {
-            System.out.println(in.nextLine());
+
+        Socket socket = new Socket("127.0.0.1", 7000);
+        System.out.println("Cliente iniciando...");
+        System.out.println("\n\n ======== Menu =====" +
+                "\n1 - readdir\n" +
+                "2 - rename\n" +
+                "3 - create\n" +
+                "4 - remove\n");
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+        while (true) {
+            Scanner in = new Scanner(System.in);
+
+            dos.writeUTF(in.nextLine());
+
+            String retorno = dis.readUTF();
+
+            System.out.println(retorno);
         }
-        in.close();
-        socket.close();
     }
 }
+
+
+      //  Socket socket = new Socket("localhost", 7000);
+      //  Scanner in = new Scanner(socket.getInputStream());
+
+
+       // while(in.hasNextLine()) {
+        //    System.out.println(in.nextLine());
+       // }
+       // in.close();
+       // socket.close();
--- src/br/edu/ifpb/gugawag/so/sockets/Server.java
+++ src/br/edu/ifpb/gugawag/so/sockets/Server.java
@@ -1,23 +1,73 @@
 package br.edu.ifpb.gugawag.so.sockets;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(7000);
-        System.out.println("==Servidor==");
-        while(true) {
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Cliente " + clientSocket.getInetAddress() + ":" + clientSocket.getPort());
 
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
-            out.println("Tchau!");
-            out.close();
-            clientSocket.close();
+        ServerSocket serverSocket = new ServerSocket(7000);
+        System.out.println("---------- Servidor iniciado ----------");
+        Socket clientSocket = serverSocket.accept();
+        DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+        DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+
+        while(true) {
+
+            String op = dis.readUTF();
+            switch(op){
+                case "1":
+
+                    System.out.println("== Enviando Lista de nomes ==");
+
+                    Path arquivo = Paths.get("C:/Users/Amanda/Documents/GitHub/nfs-sockets/arquivo.txt");
+
+                    if(Files.isReadable(arquivo))
+                        dos.writeUTF("Nome do arquivo:"+ arquivo.getFileName());
+                        System.out.println(Files.readAllLines(arquivo).toString());
+                        dos.writeUTF("Lista de nomes:"+Files.readAllLines(arquivo).toString());
+
+                case "2":
+//                    Path novo = Files.createFile(Paths.get("C:/Users/Amanda/Documents/GitHub/nfs-sockets/novo.txt"));
+                    File arquivo1 = new File("C:/Users/Amanda/Documents/GitHub/nfs-sockets/arquivo.txt");
+                    arquivo1.renameTo(new File("C:/Users/Amanda/Documents/GitHub/nfs-sockets/novo.txt"));
+
+                    dos.writeUTF("Execute o readdir novamente!");
+
+                case "3":
+
+                    Path novo = Files.createFile(Paths.get("C:/Users/Amanda/Documents/GitHub/nfs-sockets/novo.txt"));
+
+                    dos.writeUTF("O arquivo"+novo.getFileName() + "foi criado!");
+
+                case "4":
+
+                   Files.delete(Paths.get("C:/Users/Amanda/Documents/GitHub/nfs-sockets/arquivo.txt"));
+                    dos.writeUTF("Um arquivo foi deletado!");
+            }
+
+
+        }
+        
+       // while(true) {
+       //     Socket clientSocket = serverSocket.accept();
+       //     System.out.println("Cliente " + clientSocket.getInetAddress() + ":" + clientSocket.getPort());
+
+       ///     PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+        //    out.println("Tchau!");
+        //    out.close();
+         //   clientSocket.close();
         }
 
     }
-}
+
